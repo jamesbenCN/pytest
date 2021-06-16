e229d93b0ab5967ce3c2136dfa22d41290ec7b81
jamesbenCN <35627763+jamesbenCN@users.noreply.github.com>
2021-06-16 21:47:17 +0800

    Update yangWs.py

--- yangWs.py
+++ yangWs.py
@@ -1,1 +1,35 @@
 
+import tornado.ioloop
+import tornado.web
+from suds.client import Client
+
+
+def ws(request):
+    url = 'http://61.184.78.105:4588/skpayservice/skpayws.asmx?WSDL'
+    client = Client(url)
+    services = client.service
+    result = services.rews(request)
+    # print(result)
+    return result
+
+
+class mainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+    def post(self):
+        print(ws(str(self.request.body, 'utf-8')))
+        self.write("ok")
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/test", mainHandler),
+    ])
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8088)
+    print('Server started at http://127.0.0.1:8088...')
+    tornado.ioloop.IOLoop.current().start()
